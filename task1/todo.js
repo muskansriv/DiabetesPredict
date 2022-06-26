3810c74b1149dc6ab40fe22c13e1046dd1966a0a
MUSKAN SRIVASTAVA <101044910+muskansriv@users.noreply.github.com>
2022-06-27 01:41:46 +0530

    Update todo.js

--- task1/todo.js
+++ task1/todo.js
@@ -4,24 +4,18 @@ function add(){
 
     if(item.value != ""){
 	
-		// Creating element and adding value to it
 		let make_li = document.createElement("LI");
 		make_li.appendChild(document.createTextNode(item.value));
-	
-		// Adding li to ul
+
 		list_item.appendChild(make_li);
 	
 		// Reset the value of box
 		item.value=""
-		
-		// Delete a li item on click
 		make_li.onclick = function(){
 			this.parentNode.removeChild(this);
 		}
     }
     else{
-
-    // Alert msg when value of box is "" empty.
     alert("plz add a value to item");
     }
 
